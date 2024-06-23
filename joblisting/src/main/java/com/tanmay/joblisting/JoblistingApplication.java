949d85572e6a4e3d087f2fe7b750d4a84a9ef09f
Tanmay <tanmaymandal112233@gmail.com>
2024-06-23 17:12:13 +0530

    first commit

---
+++ joblisting/src/main/java/com/tanmay/joblisting/JoblistingApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.joblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblistingApplication.class, args);
+	}
+
+}
---
+++ joblisting/src/test/java/com/tanmay/joblisting/JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
